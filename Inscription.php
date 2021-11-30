2ef03e826ac68dc8fb4e33946b53ee354349651c
Nolliver <64092458+Nolliver@users.noreply.github.com>
2021-11-30 09:08:35 +0100

    Finalisation de l'inscription
    Inscription terminé
    Coté PHP:
     - Vérif que les 2 mdp sont identiques
     - Hash du mot de passe
     - Utilisateur avec profil "Tout public" par défaut (Procédure pour changer de profil à faire)
     - Pas de date d'abonnement 
    Coté HTML:
     - Légère mise en forme avec Bootstrap 5.0

--- Inscription.php
+++ Inscription.php
@@ -1,33 +1,62 @@
-<html>
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Inscription</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+</head>
+<body>
+    
 <?php
-include "connexion.php";
-$con=connect();
-if (!$con) {
-    echo "Probleme de connexion à la base";
-    exit;
- }
+//Connexion à la base de données
+    include "connexion.php";
+    $con=connect();
+    if (!$con) {
+        echo "Probleme de connexion à la base";
+        exit;
+     }
 ?>
-<h1 align="center"> Inscrivez-vous</h1>
-<form action="inscrire.php" method=POST>
-<table>
-    <tr>
-        <td>Entrez votre nom :</td><td><input type="text" name="nom" required></td></tr>
-    <tr>
-        <td>Entrez votre prenom :</td><td><input type="text" name="prenom"></td>
-    </tr>
-    <tr>
-        <td>Date de naissance : </td><td><input type=date name="dn"></td></tr>
-    <tr><td>Adresse : </td><td>N° et nom de rue : <input type="text" name="adresserue"></td></tr>
-    <tr>
-        <td /><td>Code postal : <input type="text" name="codepostal"></td>
-    </tr>
-    <tr>
-        <td>Mot de passe : </td><td><input type="password" name="mdp" required></td>
-    </tr>
-    <tr>
-        <td> Confirmez votre mot de passe : </td><td><input type="password" name="mdp"></td>
-    </tr>
-</table></br>
-<input align="center" type="submit" value="S'inscrire">
-</form>
+
+<div class="container">
+    <h1 class="text-center my-4"> Inscription</h1>
+
+    <form action="inscrire.php" method=POST class >
+    <table>
+        <tr>
+            <td>Entrez votre nom :</td>
+            <td><input type="text" name="nom" required></td>
+        </tr>
+        <tr>
+            <td>Entrez votre prenom :</td>
+            <td><input type="text" name="prenom" required></td>
+        </tr>
+        <tr>
+            <td>Date de naissance : </td>
+            <td><input type=date name="dn" required></td>
+        </tr>
+        <tr>
+            <td>Adresse : </td>
+            <td>N° et nom de rue : <input type="text" name="adresserue" required></td>
+        </tr>
+        <tr>
+            <td></td><td>Code postal : <input type="text" name="codepostal" required></td>
+        </tr>
+        <tr>
+            <td>Mot de passe : </td>
+            <td><input type="password" name="mdp" required></td>
+        </tr>
+        <tr>
+            <td> Confirmez votre mot de passe : </td>
+            <td><input type="password" name="mdp2" required></td>
+        </tr>
+    </table>
+
+    </br>
+    <input align="center" type="submit" value="S'inscrire" class="btn btn-outline-success">
+    <a href='index.php' class="btn btn-outline-danger">Retour à l'accueil</a>
+    </form>
+</div>
+
+</body>
 </html>
--- inscrire.php
+++ inscrire.php
@@ -1,40 +1,72 @@
-<html>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Inscription</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+</head>
+<body>
+    
+<h1>Inscription</h1>
+
 <?php
+//Import des fichiers nécessaires
 include "connexion.php";
 include "Fonctions.php";
+
+
+//Connexion à la base de données
 $con=connect();
 if (!$con) {
     echo "Probleme de connexion à la base";
     exit;
  }
-extract($_POST);
+
+//Récupération des informations saisies dans le formulaire
+    extract($_POST);
+
+
+//Vérification mot de passe identique
+    if ($mdp != $mdp2) {
+        echo "<h2>Erreur lors de l'inscription : </h2>";
+        echo "<p>Les 2 mots de passes ne sont pas identiques</p>";
+        echo "<a href='Inscription.php'>Retour au formulaire</a>";
+        exit;
+    } else {
+        $mdp = password_hash($mdp, PASSWORD_DEFAULT);
+    }
+
+
 //Boucle d'incrémentation du numero unique(numu) associé à chaque utilisateur
-$numu="select max(numu) as max from utilisateur";
-$resultat=pg_query($numu);
+    $numu="select max(numu) as max from utilisateur";
+    $resultat=pg_query($numu);
 
-if (!$resultat) {
-    echo " Probleme lors du lancement de la requete 1";
-    exit;
-    }
-$ligne=pg_fetch_array($resultat);
+    if (!$resultat) {
+        echo " Probleme lors du lancement de la requete 1";
+        exit;
+        }
+    $ligne=pg_fetch_array($resultat);
+
+    $id = $ligne['max']+1;
 
 
-$id = $ligne['max']+1;
 //Insertion du nouvel utilisateur dans la base
-$sql="insert into utilisateur values ($id,'$nom','$prenom','$dn','$adresserue $codepostal',".codep($dn).",NULL,".datedebutabo().");";
+    $sql="INSERT INTO utilisateur (numu, nom, prenom, dn, adresse, codep, codet, datedebutabo, password) VALUES ($id,'$nom','$prenom','$dn','$adresserue $codepostal','TP',NULL,NULL, '$mdp');";
 
-$result=pg_query($sql);
+    $result=pg_query($sql);
 
-if (!$result) {
-    echo " Probleme lors du lancement de la requete 2";
-    exit;
+    if (!$result) {
+        echo " Probleme lors du lancement de la requete 2";
+        exit;
+        }
+    else {
+        echo "<h2>Inscription validée</h2>";
+        echo "<a href='index.php'>Retour à l'accueil</a>";
     }
-else {
-    echo "Inscription validée";
-    echo "<a><href='Accueil.html'>Retour</a>";
-}
 ?>
 
 
 
+</body>
 </html>
